2e4ba55dc72a89219d9e66ad4c00332a46ec0a88
megchernekoff <megchernekoff@proton.me>
2023-04-19 09:03:38 -0700

    shuffle all categories, not just contestants

--- app.py
+++ app.py
@@ -25,9 +25,13 @@ def get_inputs():
         cursor = conn.execute("""Select * from survivor where Season = {}""".format(num))
         results = pd.DataFrame(cursor.fetchall(), columns=['Contestant', 'Age', 'From', 'Season', 'Season Name', 'Season Premise'])
         snum, sname, snprem = results.iloc[0][['Season', 'Season Name', 'Season Premise']].tolist()
-        conts, ags, fs = results['Contestant'].tolist(), results['Age'].tolist(), results['From'].tolist()
+        caf_list = list(zip(df['Contestant'], df['Age'], df['From']))
+        # conts, ags, fs = results['Contestant'].tolist(), results['Age'].tolist(), results['From'].tolist()
         if shuffle == 'Yes':
-            random.shuffle(conts)
+            random.shuffle(caf_list)
+            # random.shuffle(conts)
+        caf_uz = list(zip(*caf_list))
+        conts, ags, fs = caf_uz[0], caf_uz[1], caf_uz[2]
         return render_template('result.html', template_folder='templates', season_name=sname, season_prem=snprem, contestants=conts, ages = ags, froms = fs)
     else:
         render_template('survivor.html', template_folder='templates')
